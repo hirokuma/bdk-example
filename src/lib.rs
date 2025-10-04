c17cac3f97a64dbbb54ed7bb834b04ddf0c60f5b
hirokuma <hiro9ma@gmail.com>
2025-10-04 12:52:55 +0900

    command line

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,24 @@ pub mod segwit;
 use segwit::v1;
 use bitcoin::consensus::encode;
 
+pub fn cmd_addresses() -> Result<(), i32> {
+    Err(1)
+}
+
+pub fn cmd_newaddr() -> Result<(), i32> {
+    Err(1)
+}
+
+pub fn cmd_tx(args: &[String]) -> Result<(), i32> {
+    println!("{:?}", args);
+    Err(1)
+}
+
+pub fn cmd_spend(args: &[String]) -> Result<(), i32> {
+    println!("{:?}", args);
+    Err(1)
+}
+
 pub fn run_segwit_examples() {
     let tx = v1::segwit_v1().unwrap_or_else(|e| {
         eprintln!("Error creating segwit v1 transaction: {}", e);
@@ -10,7 +28,6 @@ pub fn run_segwit_examples() {
     });
 
     let s: Vec<u8> = encode::serialize(&tx);
-    println!("length: {}", s.len());
     let hex_str = s.iter().map(|b| format!("{:02x}", b)).collect::<String>();
     println!("{}", hex_str);
     println!("{:#?}", tx);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    bdk_starter_example::run_segwit_examples()
+use std::env;
+use bdk_starter_example as lib;
+
+fn main() -> Result<(), i32> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 || args[1] == "help" {
+        cmd_help(&args[0]);
+    } else if args[1] == "addr" {
+        lib::cmd_addresses()?
+    } else if args[1] == "newaddr" {
+        lib::cmd_newaddr()?
+    } else if args[1] == "tx" {
+        lib::cmd_tx(&args[2..])?
+    } else if args[1] == "spend" {
+        lib::cmd_spend(&args[2..])?
+    } else {
+        eprintln!("invalid options: {}", args[1]);
+        cmd_help(&args[0]);
+        return Err(1);
+    }
+    Ok(())
+}
+
+fn cmd_help(cmd: &String) {
+    println!("Usage: {} [options]", cmd);
+    println!("\nOptions:");
+    println!("  addr              Get addresses.");
+    println!("  newaddr           Get new address.");
+    println!("  tx <hex_string>   Decode transaction hex string.");
+    println!("  spend <input_hex> <out_index> <output_address> <amount_sats> <feerate>");
+    println!("                    Decode transaction hex string.");
+    println!("  help              Show this help message and exit.");
 }
